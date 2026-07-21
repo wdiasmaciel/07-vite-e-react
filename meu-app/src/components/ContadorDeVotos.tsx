09ffa4f4e0f0eb547661b7e1005c8e8e6c24cbf7
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-07-21 23:44:35 +0000

    Exemplo

--- meu-app/src/components/ContadorDeVotos.tsx
+++ meu-app/src/components/ContadorDeVotos.tsx
@@ -11,22 +11,19 @@ export default function ContadorDeVotos() {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center border border-red-300 bg-gray-100">
-      <div className="w-sm rounded-lg border 
-                    border-gray-300 bg-white p-4 shadow-md
-                    flex flex-col">
-        <h1 className="text-xl font-semibold text-gray-800">
+    <div className={styles.container}>
+      <div className={styles.cartao}>
+        <h1 className={styles.titulo}>
           Votação
         </h1>
-        <p className="mt-2 text-gray-600">
+        <p className={styles.mensagem}>
           Total de votos: {votos}
         </p>
 
         {/* Evento de clique nativo do React: */}
         <button
           onClick={adicionarVoto}
-          className="mt-4 rounded-md bg-blue-600 px-4 py-2 text-white 
-                   transition hover:bg-blue-700 self-end ml-auto"
+          className={styles.botao}
         >
           Votar
         </button>
@@ -34,3 +31,11 @@ export default function ContadorDeVotos() {
     </div>
   );
 }
+
+const styles = {
+  container: 'flex min-h-screen justify-center items-center border border-red-300 bg-gray-100',
+  cartao: 'w-sm rounded-lg border border-gray-300 bg-white p-4 shadow-md flex flex-col',
+  titulo: 'text-xl font-semibold text-gray-800',
+  mensagem: 'mt-2 text-gray-600',
+  botao: 'mt-4 rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700 self-end ml-auto'
+};
