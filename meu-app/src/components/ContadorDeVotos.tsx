b4bd3dfa08f1f5de119cccca4e6a7ee1fe071445
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-07-21 23:35:32 +0000

    Exemplo

--- meu-app/src/components/ContadorDeVotos.tsx
+++ meu-app/src/components/ContadorDeVotos.tsx
@@ -11,14 +11,21 @@ export default function ContadorDeVotos() {
   }
 
   return (
-    <div className="mx-auto my-5 max-w-sm rounded-lg border border-gray-300 bg-white p-4 shadow-sm">
-      <h1 className="text-xl font-semibold text-gray-800">Votação</h1>
-      <p className="mt-2 text-gray-600">Total de votos da chapa: {votos}</p>
+    <div className="mx-auto my-5 max-w-sm rounded-lg border 
+                    border-gray-300 bg-white p-4 shadow-md
+                    flex flex-col">
+      <h1 className="text-xl font-semibold text-gray-800">
+        Votação
+      </h1>
+      <p className="mt-2 text-gray-600">
+        Total de votos: {votos}
+      </p>
 
       {/* Evento de clique nativo do React: */}
       <button
         onClick={adicionarVoto}
-        className="mt-4 rounded-md bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
+        className="mt-4 rounded-md bg-blue-600 px-4 py-2 text-white 
+                   transition hover:bg-blue-700 self-end ml-auto"
       >
         Votar
       </button>
