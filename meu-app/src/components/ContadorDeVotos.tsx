58b52d5a1292f645194ea80040523ce8e952d4fb
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-07-21 23:39:51 +0000

    Exemplo

--- meu-app/src/components/ContadorDeVotos.tsx
+++ meu-app/src/components/ContadorDeVotos.tsx
@@ -11,24 +11,26 @@ export default function ContadorDeVotos() {
   }
 
   return (
-    <div className="mx-auto my-5 max-w-sm rounded-lg border 
+    <div className="flex min-h-screen items-center justify-center border border-red-300 bg-gray-100">
+      <div className="w-sm rounded-lg border 
                     border-gray-300 bg-white p-4 shadow-md
                     flex flex-col">
-      <h1 className="text-xl font-semibold text-gray-800">
-        Votação
-      </h1>
-      <p className="mt-2 text-gray-600">
-        Total de votos: {votos}
-      </p>
+        <h1 className="text-xl font-semibold text-gray-800">
+          Votação
+        </h1>
+        <p className="mt-2 text-gray-600">
+          Total de votos: {votos}
+        </p>
 
-      {/* Evento de clique nativo do React: */}
-      <button
-        onClick={adicionarVoto}
-        className="mt-4 rounded-md bg-blue-600 px-4 py-2 text-white 
+        {/* Evento de clique nativo do React: */}
+        <button
+          onClick={adicionarVoto}
+          className="mt-4 rounded-md bg-blue-600 px-4 py-2 text-white 
                    transition hover:bg-blue-700 self-end ml-auto"
-      >
-        Votar
-      </button>
+        >
+          Votar
+        </button>
+      </div>
     </div>
   );
 }
